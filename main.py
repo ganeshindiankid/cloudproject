a41ac3478b381395ba4e58b81fd3a2d6c40a2764
ganesh <ganeshdahiphale10@gmail.com>
2025-06-27 15:13:26 +0530

    static website

---
+++ main.py
@@ -0,0 +1,14 @@
+import http.server
+import socketserver
+import os
+
+PORT = 8000
+DIRECTORY = "New folder"
+
+os.chdir(DIRECTORY)
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
